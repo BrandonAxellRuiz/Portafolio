4b88af2d89b6c7aa43b112015933f22d1584f64f
Brandon Axell Ruiz <brandon_axell@hotmail.com>
2018-05-11 10:28:51 -0500

    Update fbSdk.js

--- js/fbSdk.js
+++ js/fbSdk.js
@@ -1,5 +1,3 @@
-<!-- Load Facebook SDK for JavaScript -->
-<div id="fb-root"></div>
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
@@ -8,11 +6,3 @@
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 
-<!-- Your customer chat code -->
-<div class="fb-customerchat"
-  attribution="setup_tool"
-  page_id="1843146869053586"
-  theme_color="#4E342E"
-  logged_in_greeting="Hola, en qué puedo ayudarte?"
-  logged_out_greeting="Hola, en qué puedo ayudarte?">
-</div>
