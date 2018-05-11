b93d676dbe9bb6505b921c6d580d528da63dbe0d
Brandon Axell Ruiz <brandon_axell@hotmail.com>
2018-05-11 10:23:53 -0500

    Create fbSdk.js

---
+++ js/fbSdk.js
@@ -0,0 +1,18 @@
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your customer chat code -->
+<div class="fb-customerchat"
+  attribution="setup_tool"
+  page_id="1843146869053586"
+  theme_color="#4E342E"
+  logged_in_greeting="Hola, en qué puedo ayudarte?"
+  logged_out_greeting="Hola, en qué puedo ayudarte?">
+</div>
